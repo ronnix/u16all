589a6e4cdc894f614ddc081b2492529bbd254d0f
Ronan Amicel <ronan.amicel@gmail.com>
2017-01-21 17:44:23 +0100

    Update neo4j APT repository URL to use HTTPS

--- version/neo4j_3_0.sh
+++ version/neo4j_3_0.sh
@@ -9,7 +9,7 @@ sudo apt-get install -y lsof
 
 # Install Neo4j
 wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
-echo 'deb http://debian.neo4j.org/repo stable/' >/tmp/neo4j.list
+echo 'deb https://debian.neo4j.org/repo stable/' >/tmp/neo4j.list
 sudo mv /tmp/neo4j.list /etc/apt/sources.list.d
 sudo apt-get update
 sudo apt-get install neo4j=${VERSION}
